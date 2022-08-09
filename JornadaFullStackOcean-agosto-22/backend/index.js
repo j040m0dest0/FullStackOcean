a6284595ebd7fa743fc40a764d111dc256189e73
João Victor Modesto da Cruz <joao.modesto@progen.com.br>
2022-08-09 12:00:08 -0300

    firstTime running the server

--- JornadaFullStackOcean-agosto-22/backend/index.js
+++ JornadaFullStackOcean-agosto-22/backend/index.js
@@ -1,1 +1,14 @@
-console.log("Hello world");
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Humble project');
+});
+
+app.get('/oi', function (req, res) {
+    res.send('Olá Mundo');
+  });
+
+app.listen(3000, () =>
+  console.log("Servidor rodando em http://localhost:3000/")
+);
