caf465b914d3a0688f50141fb39097c6b21e0fe8
João Victor Modesto da Cruz <joao.modesto@progen.com.br>
2022-08-11 12:12:20 -0300

    criação dos endpoints

--- JornadaFullStackOcean-agosto-22/backend/index.js
+++ JornadaFullStackOcean-agosto-22/backend/index.js
@@ -1,13 +1,57 @@
 const express = require('express');
 const app = express();
 
+
+
+app.use(express.json());
+
 app.get('/', function (req, res) {
   res.send('Humble project');
 });
 
 app.get('/oi', function (req, res) {
     res.send('Olá Mundo');
+});
+
+
+const lista = [
+  {
+    id: 1,
+    nome: "João",
+    pontos: 90,
+  },
+  {
+    id: 2,
+    nome: "Carol",
+    pontos: 20,
+  },
+  {
+    id: 3,
+    nome: "Fabio",
+    pontos: 70,
+  },
+];
+
+
+//[GET] Read All
+app.get("/pontuacoes", function(req, res) {
+  res.send(lista);
+});
+
+//[POST] Create
+app.post("/pontuacoes", function (req, res){
+  const item = req.body;
+
+  lista.push({
+    id: lista.length + 1,
+    nome: item.nome,
+    pontos: item.pontos,
+
   });
+  console.log("Item criado com sucesso");
+
+  res.send("criar uma pontuação");
+});
 
 app.listen(3000, () =>
   console.log("Servidor rodando em http://localhost:3000/")
